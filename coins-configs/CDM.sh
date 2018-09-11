749ea2d056684f476405ebf2b29f4291f1ea5847
Aleksandr Bovtruk <aleks@MacBook-Pro-Aleksandr-3.local>
2018-09-11 17:52:47 +0800

    added CDM config

--- coins-configs/CDM.sh
+++ coins-configs/CDM.sh
@@ -10,5 +10,5 @@ COIN_DAEMON="${COIN_FOLDER}/condominiumd"
 COIN_CLI="${COIN_FOLDER}/condominium-cli"
 
 function coin_custom_comile() {
-    cp ./* $COIN_FOLDER
+    cp ./linux1210/* $COIN_FOLDER
 }

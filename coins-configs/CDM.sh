2062148dbcd5fd820a4bd82bb26c38c78c4577fa
Aleksandr Bovtruk <aleks@MacBook-Pro-Aleksandr-3.local>
2018-09-11 17:27:18 +0800

    added CDM config

---
+++ coins-configs/CDM.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+COIN_REPO='https://github.com/condominium/CondominiumCore/releases/download/v1.2.1.0/CDM-linux1210.zip'
+COIN_ZIP='CDM-linux1210.zip'
+COIN_SENTINEL_REPO='https://github.com/goacoincore/sentinel.git'
+COIN_SENTINEL_CONF_PARAM='goacoin_conf'
+CONFIG_FILE='condominium.conf'
+COIN_PORT=33588
+COIN_DAEMON="${COIN_FOLDER}/condominiumd"
+COIN_CLI="${COIN_FOLDER}/condominium-cli"
+
+function coin_custom_comile() {
+    cp * $COIN_FOLDER
+}

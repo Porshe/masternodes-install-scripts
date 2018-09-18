d6226059eca276b57af2a13452b705f7597aa554
Aleksandr Bovtruk <aleks@MacBook-Pro-Aleksandr-3.local>
2018-09-18 18:26:14 +0800

    updated MERO to v.1.0.2

--- coins-configs/MERO.sh
+++ coins-configs/MERO.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-COIN_REPO='https://github.com/Mero-HH/mero/archive/v1.0.1.tar.gz'
-COIN_ZIP='v1.0.1.tar.gz'
+COIN_REPO='https://github.com/Mero-HH/mero/archive/v1.0.2.tar.gz'
+COIN_ZIP='v1.0.2.tar.gz'
 CONFIG_FILE='mero.conf'
 COIN_PORT=14550
 COIN_DAEMON="${COIN_FOLDER}/merod"
 COIN_CLI="${COIN_FOLDER}/mero-cli"
 
 function coin_custom_comile() {
-    cd ./mero-1.0.1
+    cd ./mero-1.0.2
     ./autogen.sh >/dev/null 2>&1
     ./configure --disable-tests --disable-gui-tests >/dev/null 2>&1
 
